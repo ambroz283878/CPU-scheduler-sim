9510e607925e8e167e6ee3cbfc9fc30edcd06edb
ambroz <283878@student.pwr.edu.pl>
2026-06-02 15:34:01 +0200

    Removed unnecessary file

--- queue.py
+++
@@ -1,9 +0,0 @@
-class Queue():
-    def __init__(self):
-        self.elements = []
-    def advanceElement(self):
-        return self.elements.pop(0)
-    def addElement(self, element):
-        self.elements.insert(len(self.elements),element)
-
-   
